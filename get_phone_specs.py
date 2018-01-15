a9f4aa315157e60b55c14b0e049743f6eac4ead8
John Santhosh <jsanthos@asu.edu>
2018-01-14 22:14:34 -0700

    Write the phone specs to specs.txt for now.

--- get_phone_specs.py
+++ get_phone_specs.py
@@ -1,99 +1,39 @@
-import urllib3
-from bs4 import BeautifulSoup
 import sys
-
-urllib3.disable_warnings()
-
-def fetchTextContent(element):
-    if element:
-        return element.text
-    else:
-        ""
+import scrape
 
 storage_path = 'specs/'
 file_name = 'specs.txt'
 phone_list = 'phone_list.txt'
 base_url = "https://www.gsmarena.com/"
 
-try:
-    with open(phone_list) as f:
-        phones = f.readlines()
-    phones = [x.strip().replace('\n', '') for x in phones]
-except IOError:
-    print('\n' + 'No phone list to iterate. ' + '\n')
-    sys.exit(1)
 
-f = open(storage_path + file_name, 'w')
-for phone in phones:
-    print(phone)
-    http = urllib3.PoolManager()
-    page = http.request('GET', base_url + phone)
-    spec_json = {}
-    if page.status == 200:
-        soup = BeautifulSoup(page.data, 'lxml')
-        spec = soup.find_all('div', id='specs-list')
-        spec_soup = BeautifulSoup(str(spec), 'lxml')
-        #network
-        spec_json['network_2g'] = fetchTextContent(spec_soup.find('td',{"data-spec" : "net2g"}))
-        spec_json['network_3g'] = fetchTextContent(spec_soup.find('td', {"data-spec": "net3g"}))
-        spec_json['network_4g'] = fetchTextContent(spec_soup.find('td', {"data-spec": "net4g"}))
-        spec_json['network_speed'] = fetchTextContent(spec_soup.find('td', {"data-spec": "speed"}))
-        spec_json['network_gprs_support'] = fetchTextContent(spec_soup.find('td', {"data-spec": "gprstext"}))
-        spec_json['network_edge_support'] = fetchTextContent(spec_soup.find('td', {"data-spec": "edge"}))
-        #launch
-        spec_json['launch_announced'] = fetchTextContent(spec_soup.find('td', {"data-spec": "year"}))
-        spec_json['launch_availability'] = fetchTextContent(spec_soup.find('td', {"data-spec": "status"}))
-        #body
-        spec_json['body_dimensions'] = fetchTextContent(spec_soup.find('td', {"data-spec": "dimensions"}))
-        spec_json['body_weight'] = fetchTextContent(spec_soup.find('td', {"data-spec": "weight"}))
-        spec_json['body_build'] = fetchTextContent(spec_soup.find('td', {"data-spec": "build"}))
-        spec_json['body_sim'] = fetchTextContent(spec_soup.find('td', {"data-spec": "sim"}))
-        spec_json['body_other'] = fetchTextContent(spec_soup.find('td', {"data-spec": "bodyother"}))
-        #display
-        spec_json['display_type'] = fetchTextContent(spec_soup.find('td', {"data-spec": "displaytype"}))
-        spec_json['display_size'] = fetchTextContent(spec_soup.find('td', {"data-spec": "displaysize"}))
-        spec_json['display_resolution'] = fetchTextContent(spec_soup.find('td', {"data-spec": "displayresolution"}))
-        spec_json['display_protection'] = fetchTextContent(spec_soup.find('td', {"data-spec": "displayprotection"}))
-        spec_json['display_other'] = fetchTextContent(spec_soup.find('td', {"data-spec": "displayother"}))
-        #platform
-        spec_json['platform_os'] = fetchTextContent(spec_soup.find('td', {"data-spec": "os"}))
-        spec_json['platform_chipset'] = fetchTextContent(spec_soup.find('td', {"data-spec": "chipset"}))
-        spec_json['platform_cpu'] = fetchTextContent(spec_soup.find('td', {"data-spec": "cpu"}))
-        spec_json['platform_gpu'] = fetchTextContent(spec_soup.find('td', {"data-spec": "gpu"}))
-        #memory
-        spec_json['memory_slot'] = fetchTextContent(spec_soup.find('td', {"data-spec": "memoryslot"}))
-        spec_json['internal_memory'] = fetchTextContent(spec_soup.find('td', {"data-spec": "internalmemory"}))
-        #camera
-        spec_json['camera_primary'] = fetchTextContent(spec_soup.find('td', {"data-spec": "cameraprimary"}))
-        spec_json['camera_features'] = fetchTextContent(spec_soup.find('td', {"data-spec": "camerafeatures"}))
-        spec_json['camera_video'] = fetchTextContent(spec_soup.find('td', {"data-spec": "camera_video"}))
-        spec_json['camera_secondary'] = fetchTextContent(spec_soup.find('td', {"data-spec": "camerasecondary"}))
-        #comms
-        spec_json['comms_wlan'] = fetchTextContent(spec_soup.find('td', {"data-spec": "wlan"}))
-        spec_json['comms_bluetooth'] = fetchTextContent(spec_soup.find('td', {"data-spec": "bluetooth"}))
-        spec_json['comms_gps'] = fetchTextContent(spec_soup.find('td', {"data-spec": "gps"}))
-        spec_json['comms_nfc'] = fetchTextContent(spec_soup.find('td', {"data-spec": "nfc"}))
-        spec_json['comms_radio'] = fetchTextContent(spec_soup.find('td', {"data-spec": "radio"}))
-        spec_json['comms_usb'] = fetchTextContent(spec_soup.find('td', {"data-spec": "usb"}))
-        #features
-        spec_json['features_sensor'] = fetchTextContent(spec_soup.find('td', {"data-spec": "sensor"}))
-        spec_json['features_other'] = fetchTextContent(spec_soup.find('td', {"data-spec": "featuresother"}))
-        #battery
-        spec_json['battery_description'] = fetchTextContent(spec_soup.find('td', {"data-spec": "batdescription1"}))
-        spec_json['battery_talk_time'] = fetchTextContent(spec_soup.find('td', {"data-spec": "battalktime1"}))
-        spec_json['battery_music_playback'] = fetchTextContent(spec_soup.find('td', {"data-spec": "batmusicplayback1"}))
-        spec_json['comms_usb'] = fetchTextContent(spec_soup.find('td', {"data-spec": "usb"}))
-        #misc
-        spec_json['misc_colors'] = fetchTextContent(spec_soup.find('td', {"data-spec": "misccolors"}))
-        spec_json['misc_sar_us'] = fetchTextContent(spec_soup.find('td', {"data-spec": "sar-us"}))
-        spec_json['misc_sar_eu'] = fetchTextContent(spec_soup.find('td', {"data-spec": "sar-eu"}))
-        spec_json['misc_price'] = fetchTextContent(spec_soup.find('td', {"data-spec": "price"}))
-        #benchmarks
-        spec_json['bench_tbench'] = fetchTextContent(spec_soup.find('td', {"data-spec": "tbench"}))
-
-        #TODO update database
-        print(str(spec_json))
-f.close
+def fetch_phone_list():
+    try:
+        with open(phone_list) as f:
+            phones = f.readlines()
+        phones = [x.strip().replace('\n', '') for x in phones]
+    except IOError:
+        print('\n' + 'No phone list to iterate. ' + '\n')
+        sys.exit(1)
+    return phones
 
 
+def main():
+    phones = fetch_phone_list()
+    scrape_obj = scrape.Scrape()
 
+    # TODO remove
+    f = open(storage_path + file_name, 'w')
+
+    for phone in phones:
+        print(phone)
+        page = scrape_obj.scrape(base_url + phone)
+        # TODO update db
+        f.write(str(scrape_obj.extract_phone_specs(page)) + "\n")
+
+    # TODO remove
+    f.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ scrape.py
@@ -0,0 +1,92 @@
+from bs4 import BeautifulSoup
+import urllib3
+import sys
+
+
+class Scrape:
+    def __init__(self):
+        urllib3.disable_warnings()
+        self.http = urllib3.PoolManager()
+
+    def scrape(self, url):
+        try:
+            page = self.http.request('GET', url)
+        except urllib3.exceptions.HTTPError as e:
+            print("Http Error: "+str(e.reason))
+            sys.exit(1)
+        return page
+
+    def fetch_text_content(self, element):
+        if element:
+            return element.text
+        else:
+            ""
+
+    def extract_phone_specs(self, page):
+        if page.status == 200:
+            soup = BeautifulSoup(page.data, 'lxml')
+            spec = soup.find_all('div', id='specs-list')
+            spec_soup = BeautifulSoup(str(spec), 'lxml')
+            spec_json = dict()
+            # network
+            spec_json['network_2g'] = self.fetch_text_content(spec_soup.find('td', {"data-spec": "net2g"}))
+            spec_json['network_3g'] = self.fetch_text_content(spec_soup.find('td', {"data-spec": "net3g"}))
+            spec_json['network_4g'] = self.fetch_text_content(spec_soup.find('td', {"data-spec": "net4g"}))
+            spec_json['network_speed'] = self.fetch_text_content(spec_soup.find('td', {"data-spec": "speed"}))
+            spec_json['network_gprs_support'] = self.fetch_text_content(spec_soup.find('td', {"data-spec": "gprstext"}))
+            spec_json['network_edge_support'] = self.fetch_text_content(spec_soup.find('td', {"data-spec": "edge"}))
+            # launch
+            spec_json['launch_announced'] = self.fetch_text_content(spec_soup.find('td', {"data-spec": "year"}))
+            spec_json['launch_availability'] = self.fetch_text_content(spec_soup.find('td', {"data-spec": "status"}))
+            # body
+            spec_json['body_dimensions'] = self.fetch_text_content(spec_soup.find('td', {"data-spec": "dimensions"}))
+            spec_json['body_weight'] = self.fetch_text_content(spec_soup.find('td', {"data-spec": "weight"}))
+            spec_json['body_build'] =self.fetch_text_content(spec_soup.find('td', {"data-spec": "build"}))
+            spec_json['body_sim'] =self.fetch_text_content(spec_soup.find('td', {"data-spec": "sim"}))
+            spec_json['body_other'] =self.fetch_text_content(spec_soup.find('td', {"data-spec": "bodyother"}))
+            # display
+            spec_json['display_type'] =self.fetch_text_content(spec_soup.find('td', {"data-spec": "displaytype"}))
+            spec_json['display_size'] =self.fetch_text_content(spec_soup.find('td', {"data-spec": "displaysize"}))
+            spec_json['display_resolution'] =self.fetch_text_content(
+                spec_soup.find('td', {"data-spec": "displayresolution"}))
+            spec_json['display_protection'] =self.fetch_text_content(
+                spec_soup.find('td', {"data-spec": "displayprotection"}))
+            spec_json['display_other'] =self.fetch_text_content(spec_soup.find('td', {"data-spec": "displayother"}))
+            # platform
+            spec_json['platform_os'] =self.fetch_text_content(spec_soup.find('td', {"data-spec": "os"}))
+            spec_json['platform_chipset'] =self.fetch_text_content(spec_soup.find('td', {"data-spec": "chipset"}))
+            spec_json['platform_cpu'] =self.fetch_text_content(spec_soup.find('td', {"data-spec": "cpu"}))
+            spec_json['platform_gpu'] =self.fetch_text_content(spec_soup.find('td', {"data-spec": "gpu"}))
+            # memory
+            spec_json['memory_slot'] =self.fetch_text_content(spec_soup.find('td', {"data-spec": "memoryslot"}))
+            spec_json['internal_memory'] =self.fetch_text_content(spec_soup.find('td', {"data-spec": "internalmemory"}))
+            # camera
+            spec_json['camera_primary'] =self.fetch_text_content(spec_soup.find('td', {"data-spec": "cameraprimary"}))
+            spec_json['camera_features'] =self.fetch_text_content(spec_soup.find('td', {"data-spec": "camerafeatures"}))
+            spec_json['camera_video'] =self.fetch_text_content(spec_soup.find('td', {"data-spec": "camera_video"}))
+            spec_json['camera_secondary'] =self.fetch_text_content(spec_soup.find('td', {"data-spec": "camerasecondary"}))
+            # comms
+            spec_json['comms_wlan'] =self.fetch_text_content(spec_soup.find('td', {"data-spec": "wlan"}))
+            spec_json['comms_bluetooth'] =self.fetch_text_content(spec_soup.find('td', {"data-spec": "bluetooth"}))
+            spec_json['comms_gps'] =self.fetch_text_content(spec_soup.find('td', {"data-spec": "gps"}))
+            spec_json['comms_nfc'] =self.fetch_text_content(spec_soup.find('td', {"data-spec": "nfc"}))
+            spec_json['comms_radio'] =self.fetch_text_content(spec_soup.find('td', {"data-spec": "radio"}))
+            spec_json['comms_usb'] =self.fetch_text_content(spec_soup.find('td', {"data-spec": "usb"}))
+            # features
+            spec_json['features_sensor'] =self.fetch_text_content(spec_soup.find('td', {"data-spec": "sensor"}))
+            spec_json['features_other'] =self.fetch_text_content(spec_soup.find('td', {"data-spec": "featuresother"}))
+            # battery
+            spec_json['battery_description'] =self.fetch_text_content(
+                spec_soup.find('td', {"data-spec": "batdescription1"}))
+            spec_json['battery_talk_time'] =self.fetch_text_content(spec_soup.find('td', {"data-spec": "battalktime1"}))
+            spec_json['battery_music_playback'] =self.fetch_text_content(
+                spec_soup.find('td', {"data-spec": "batmusicplayback1"}))
+            spec_json['comms_usb'] =self.fetch_text_content(spec_soup.find('td', {"data-spec": "usb"}))
+            # misc
+            spec_json['misc_colors'] =self.fetch_text_content(spec_soup.find('td', {"data-spec": "misccolors"}))
+            spec_json['misc_sar_us'] =self.fetch_text_content(spec_soup.find('td', {"data-spec": "sar-us"}))
+            spec_json['misc_sar_eu'] =self.fetch_text_content(spec_soup.find('td', {"data-spec": "sar-eu"}))
+            spec_json['misc_price'] =self.fetch_text_content(spec_soup.find('td', {"data-spec": "price"}))
+            # benchmarks
+            spec_json['bench_tbench'] =self.fetch_text_content(spec_soup.find('td', {"data-spec": "tbench"}))
+            return spec_json
