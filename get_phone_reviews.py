2827c87f87a7672e05eea6fc0913e10ef960ff59
John Santhosh <jsanthos@asu.edu>
2018-02-10 03:33:32 -0700

    Changes for extracting flipkart reviews.

--- get_phone_reviews.py
+++ get_phone_reviews.py
@@ -3,32 +3,47 @@ import scrape
 import mongo
 from utils import fetch_phone_list
 from properties import REVIEW_STORAGE_PATH
-from properties import REVIEWS_FILE_NAME
 from properties import AMAZON_BASE_URL
 from properties import AMAZON_REVIEW_URL_1
+from properties import FLIPKART_REQUEST_FIELDS
+from properties import HEADERS
+from properties import FLIPKART_REVIEW_URL
+
 
 def main():
-    scrape_obj = scrape.Scrape()
     phones = fetch_phone_list()
-    print('Fetching reviews from amazon : ')
 
+    get_amazon_reviews(phones)
+    get_flipkart_reviews(phones)
+
+
+def get_amazon_reviews(phones):
+    scrape_obj = scrape.Scrape()
     for phone in phones:
-        # TODO remove
-        f = open(REVIEW_STORAGE_PATH + phone[0] + '.txt', 'a', encoding='utf-8')
         count = 1
-        page = scrape_obj.scrape(AMAZON_BASE_URL + phone[2] + AMAZON_REVIEW_URL_1 + str(count))
-        print(phone[0])
+        page = scrape_obj.scrape(AMAZON_BASE_URL + phone[2] + AMAZON_REVIEW_URL_1 + str(count), '', '')
+        print('Fetching amazon reviews for %s' % phone[0])
         while page.status == 200:
-            # TODO update db
             extracted_reviews = scrape_obj.extract_amazon_reviews(page)
             if not extracted_reviews:
                 break
-            f.write(str(extracted_reviews))
             mongo.update_phone_reviews(extracted_reviews, phone[0])
             count += 1
-            page = scrape_obj.scrape(AMAZON_BASE_URL + phone[2] + AMAZON_REVIEW_URL_1 + str(count))
-        # TODO remove
-        f.close()
+            page = scrape_obj.scrape(AMAZON_BASE_URL + phone[2] + AMAZON_REVIEW_URL_1 + str(count), '', '')
+
+
+def get_flipkart_reviews(phones):
+    scrape_obj = scrape.Scrape()
+    fields = FLIPKART_REQUEST_FIELDS
+    for phone in phones:
+        fields['productId'] = phone[3]
+        page = scrape_obj.scrape(FLIPKART_REVIEW_URL, fields, HEADERS)
+        print('Fetching flipkart reviews for %s' % phone[0])
+        if page.status == 200:
+            extracted_reviews = scrape_obj.extract_flipkart_reviews(page)
+            if not extracted_reviews:
+                break
+            mongo.update_phone_reviews(extracted_reviews, phone[0])
 
 if __name__ == "__main__":
     main()
--- properties.py
+++ properties.py
@@ -5,7 +5,9 @@ PHONE_LIST = 'phone_list.txt'
 GSM_ARENA_BASE_URL = "https://www.gsmarena.com/"
 # Reviews
 REVIEW_STORAGE_PATH = 'reviews/'
-# Filename to be changed for each phone.
 REVIEWS_FILE_NAME = 'reviews.txt'
 AMAZON_BASE_URL = 'https://www.amazon.com/'
 AMAZON_REVIEW_URL_1 = '/ref=cm_cr_arp_d_paging_btm?ie=UTF8&reviewerType=all_reviews&pageNumber='
+FLIPKART_REVIEW_URL = 'https://www.flipkart.com/api/3/product/reviews'
+FLIPKART_REQUEST_FIELDS = {"productId": "", "count": "5000", "ratings": "ALL", "reviewerType":"ALL", "sortOrder": "MOST_HELPFUL"}
+HEADERS = ({"x-user-agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.92 Safari/537.36 FKUA/website/41/website/Desktop"})
